9eb2ef19a0df8320686a5f2f693bd2310a47b413
Maxud <m.daudov@corp.mail.ru>
2022-04-10 17:40:05 +0300

    - add linter

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -166,7 +166,8 @@ func (d *Discovery) newDirCheck(entry fs.DirEntry, dto methodDto, curDepth int) 
 				return []types.FileInfo{}
 			}
 			d.log.Info("Got new children ", zap.String("path", path), zap.String("path", path))
-			return []types.FileInfo{types.FileInfo{info, path}}
+			fileInfo := types.FileInfo{info, path}
+			return []types.FileInfo{fileInfo}
 		}
 	}
 	return []types.FileInfo{}
